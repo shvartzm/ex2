dd5cab5269b862da146994a0fdf9b3e0459eefaf
Maorsh <69312417+shvartzm@users.noreply.github.com>
2025-11-20 17:36:04 +0200

    scans

--- ex2.c
+++ ex2.c
@@ -1,6 +1,6 @@
 /******************
-Name:
-ID:
+Name: Maor Shvartz
+ID: 216763235
 Assignment:
 *******************/
 
@@ -23,6 +23,22 @@ int main()
     // TASK 6: EXIT
 
     // dont forget through all the necessary parts to validate invalid input.
+    int pick = 0;
+    do
+    {
+        printf("Welcome to our games, please choose an option:\n");
+        printf("1. Ducky's Unity Game");
+        printf("2. The Memory Game");
+        printf("3. Professor Pat's Power Calculation");
+        printf("4. The Duck Parade");
+        printf("5. The Mystery of the Repeated Digits");
+        printf("6. Good Night Ducks");
+        scanf("%d", &pick);
+    } while (pick != 6);
+    
+
+
+
 
     return 0;
 }
